bf7cab0ae3fc356f4d1475025a0f4a11b497f594
Heroesluk <Heroesluk@gmail.com>
2020-01-23 00:50:22 +0100

    dziala gituwa

--- Main_app.py
+++ Main_app.py
@@ -2,6 +2,45 @@ import tkinter as tk
 
 LARGE_FONT = ("Verdana", 25)
 SMOLL_FONT = ("Verdana", 13)
+def matlibb(root):
+    import tkinter
+
+    from matplotlib.backends.backend_tkagg import (
+        FigureCanvasTkAgg, NavigationToolbar2Tk)
+    # Implement the default Matplotlib key bindings.
+    from matplotlib.backend_bases import key_press_handler
+    from matplotlib.figure import Figure
+
+    import numpy as np
+
+    fig = Figure(figsize=(5, 4), dpi=100)
+    t = np.arange(0, 3, .01)
+    fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+
+    canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+    toolbar = NavigationToolbar2Tk(canvas, root)
+    toolbar.update()
+    canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+    def on_key_press(event):
+        print("you pressed {}".format(event.key))
+        key_press_handler(event, canvas, toolbar)
+
+    canvas.mpl_connect("key_press_event", on_key_press)
+
+    def _quit():
+        root.quit()  # stops mainloop
+        root.destroy()  # this is necessary on Windows to prevent
+        # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+    button = tkinter.Button(master=root, text="Quit", command=_quit)
+    button.pack(side=tkinter.BOTTOM)
+    # If you put root.destroy() here, it will cause an error if the window is
+    # closed with the window manager.
+
 
 class SeaofBTCapp(tk.Tk):
 
@@ -111,15 +150,9 @@ class GraphWidget(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.configure(background='green')
 
-        self.label = tk.Label(self, text="Tutaj bedzie stonk graph", font=LARGE_FONT)
-        self.label.grid(row=0, column=0)
-
-        self.label = tk.Label(self, text="Tutaj tez stonk graph", font=LARGE_FONT)
-        self.label.grid(row=0, column=1)
-
-        self.label = tk.Label(self, text="I tu", font=LARGE_FONT)
-        self.label.grid(row=0, column=2)
-
+        self.spread_frame = tk.Frame(self)
+        matlibb(self.spread_frame)
+        self.spread_frame.grid(row=0, columnspan=2)
 
 
 class DayWeatherWidget(tk.Frame):
